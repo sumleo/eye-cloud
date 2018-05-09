0f59510251163ec05bef0e5f1426ea95e9443ec2
sumleo <992252973@qq.com>
2018-05-09 23:26:02 +0800

    mock version 1.1 for Dr.Li requirements

--- src/components/SiderMenu/SiderMenu.js
+++ src/components/SiderMenu/SiderMenu.js
@@ -214,7 +214,7 @@ export default class SiderMenu extends PureComponent {
       >
         <div className={styles.logo} key="logo">
           <Link to="/">
-            <img src={logo} alt="logo" />
+            {/*<img src={logo} alt="logo" />*/}
             <h1>Eye Cloud</h1>
           </Link>
         </div>
